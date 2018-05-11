92e5ad74370b000440c5ff03682aaa531b0707c2
Phuong Dang <phuongdang@admins-MacBook-Air-2.local>
2018-05-11 17:15:12 +0700

    add option to change color of title

--- public/toggle.js
+++ public/toggle.js
@@ -1,14 +1,18 @@
 const button = document.getElementById("secret");
 const nav = document.getElementById("sidebar");
+const title = document.getElementById("main-title");
 const colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'brown'];
+const colorLetterArray = ['redLetter', 'orangeLetter', 'yellowLetter', 'greenLetter', 'blueLetter', 'brownLetter'];
 let i = 1;
 
 button.addEventListener("click", function() {
     if (i < colorArray.length) {
         nav.classList.replace(colorArray[i - 1], colorArray[i]);
+        title.classList.replace(colorLetterArray[i - 1], colorLetterArray[i]);        
         i++; 
     } else {
         i = 0;
-        nav.classList.replace(colorArray[colorArray.length - 1], colorArray[i])
+        nav.classList.replace(colorArray[colorArray.length - 1], colorArray[i]);
+        title.classList.replace(colorLetterArray[colorLetterArray.length - 1], colorLetterArray[i]);        
     }
 });
